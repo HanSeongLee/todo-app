40b5932d4fa2f551147b31efc3849b120243dbc9
HanSeongLee <jkhanseong@gmail.com>
2023-02-24 21:09:44 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Header from 'components/Header';
+import TodoContainer from 'containers/TodoContainer';
+import Container from 'components/Container';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <main className={styles.main}>
+                <Header />
+                <Container>
+                    <TodoContainer className={styles.todoContainer} />
+                </Container>
+            </main>
+        </>
+    );
 }
 
 export default Home
